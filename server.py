130f26efc9368fcc77e4baef7eed54661832b44c
Ariel Nunez <ingenieroariel@gmail.com>
2010-05-18 18:40:35 +0000

    Added Duration custom response header

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from wsgiref.validate import validator
 from scrap import get_sitio
 import re
 import redis
+import time
 
 DIGITS = re.compile('\d+')
 cache = redis.Redis(host='localhost', port=6379, db=0)
@@ -23,16 +24,20 @@ def donde(cedula):
 #@validator
 def app(environ, start_response):
     """Simplest possible application object"""
+    t1 = time.time()
     path_info = environ['PATH_INFO']
     digitos= DIGITS.findall(path_info)
-    data = 'Hello, World!\n'
+    data = '{"error": "Cagada"}'
     status = '200 OK'
     if len(digitos)==1:
         cedula = digitos[0]
         data =donde(cedula)
+    t2 = time.time()
+    duration = str("%0.3f ms" % ((t2-t1)*1000.0,))
     response_headers = [
         ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-Length', str(len(data))),
+        ('Duration', duration)
     ]
     start_response(status, response_headers)
     return iter([data])
