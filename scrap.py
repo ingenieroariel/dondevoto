d4f26d69823d13d7cad182ddad72d2f2b781bc64
Ariel Núñez <ingenieroariel@gmail.com>
2010-05-18 13:59:56 -0500

    Removed pycurl dependency

--- scrap.py
+++ scrap.py
@@ -1,47 +1,41 @@
-import pycurl
+# -*- coding: utf-8 -
 import urllib2
-from BeautifulSoup import BeautifulSoup
 import json
+from BeautifulSoup import BeautifulSoup
 
 BASE_URL="http://www3.registraduria.gov.co/censo/_censoresultado.php?nCedula=%s"
 CEDULAS="cedulas.txt"
 OUTPUT="votacion.json"
 USER_AGENT="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
 
-class Info:
-    def __init__(self):
-        self.contents = ''
-
-    def body_callback(self, buf):
-        self.contents = self.contents + buf
-
-    def data(self):
-        soup = BeautifulSoup(self.contents)
-        table = soup("td")
-        items=[]
-        for entry in table:
-            try:
-                if entry.string is not None:
-                    items.append(entry.string.strip())
-            except AttributeError:
-                pass
-        return items
+def parse(data):
+    """ Toma la página completa y le saca la información relevante"""
+    soup = BeautifulSoup(data)
+    table = soup("td")
+    items=[]
+    for entry in table:
+        try:
+            if entry.string is not None:
+                items.append(entry.string.strip())
+        except AttributeError:
+            pass
+    return items
 
 
 def get_sitio(cedula, text=False):
+    """ Se conecta con la registraduría y entrega un objecto JSON con la info"""
     url = BASE_URL % cedula
-    t = Info()
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(pycurl.USERAGENT, USER_AGENT)
-    c.setopt(c.WRITEFUNCTION, t.body_callback)
-    c.perform()
-    c.close()
-    data = {cedula: t.data()}
+    request = urllib2.Request(url)
+    opener = urllib2.build_opener()  
+    # sino se agrega un USER_AGENT valido, el servidor se queja porque no
+    # soporta frames :(
+    request.add_header('User-Agent', USER_AGENT)
+    data = opener.open(request).read()
+    sitio = {cedula: parse(data)}
     if not text:
-        return data
+        return sitio
     else:
-        return json.dumps(data) 
+        return json.dumps(sitio) 
 
 if __name__=="__main__":
     salida = {}
